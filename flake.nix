9bd8c40d0607882bee8f3b93061e6d9c8994da08
Melody Henrich <mixolydianmel@proton.me>
2025-07-13 15:12:24 -0400

    Scaffold initial flake structure

--- flake.nix
+++ flake.nix
@@ -1,15 +1,27 @@
 {
-  description = "A very basic flake";
+  description = "My work through the famous \"Ray Tracing in One Weekend\" series. ";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = import nixpkgs {
+      inherit system;
+    };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    pname = "ray-tracing";
+    version = "0.1.0";
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+    buildInputs = with pkgs; [];
+    nativeBuildInputs = with pkgs; [];
+  in
+  {
+    devShells.default = pkgs.mkShell {
+      inherit pname version buildInputs nativeBuildInputs;
+    };
+  });
 }
